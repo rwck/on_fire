bc949050b3bfb2eca058dcdf2c64405b31fbff7b
Donald Wade <rwckdw@gmail.com>
2015-11-18 08:31:54 +1100

    created database seed file

--- PlanetExpress/db/seeds.rb
+++ PlanetExpress/db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+reservations = Reservation.create([
+  {
+    seat_reserved: A2,
+    user_id: 23,
+    flight_id: 1
+  },
+  {
+    seat_reserved: X2,
+    user_id: 33,
+    flight_id: 2
+  }
+
+])
+
+flights = Flight.create([
+  {
+    number: 5454,
+    origin: "san diego",
+    destination: "toronto",
+    seating_array: [[0,0,0,0,0][0,0,0,0,0]]
+    airplane_id: 1
+  },
+  {
+    number: 88,
+    origin: "brisbane",
+    destination: "the moon",
+    seating_array: [[0,0,0,0,0][0,0,0,0,0]]
+    airplane_id: 2
+  }
+])
+
+user = User.create([
+  {
+    name: "Jonty",
+    password: "bad_password",
+    user_type: "admin"
+  },
+  {
+    name: "Olivia",
+    password: "i'm a toddler",
+    user_type: "toddler"
+  }
+])
+
+planes = Plane.create([
+  {
+    name: "737",
+    row: 6,
+    column: 50
+  },
+  {
+    name: "747",
+    row: 8,
+    column: 150
+  },
+  {
+    name: "fighter_jet",
+    row: 1,
+    column: 2
+  },
+  {
+    name: "airbus",
+    row: 8,
+    column: 300
+  }
+])
