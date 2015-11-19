00b4b52e3fe261f54a3449dbd487f69bb5920ab7
Donald Wade <rwckdw@gmail.com>
2015-11-19 21:11:37 +1100

    tweaking event listener

--- PlanetExpress/app/assets/javascripts/views/probablyforyilan.js
+++ PlanetExpress/app/assets/javascripts/views/probablyforyilan.js
@@ -14,19 +14,20 @@ app.Flight_view = Backbone.View.extend({
   seatClicked: function(event) {
     el = "#table";
     console.log(this);
-    alert("booo!")
-    console.log(event);
+    // alert("booo!")
+    var bookedSeat = event.target.id;
+    console.log(bookedSeat);
   },
 
-  drawTable: function(event) {
-    // console.log(event.target.id);
-    console.log("drawing table");
-    var view = this;
-    testview = this;
-
-    // this.collection.where({number: })
-
-    console.log(this_destination);
+  // drawTable: function(event) {
+  //   // console.log(event.target.id);
+  //   console.log("drawing table");
+  //   var view = this;
+  //   testview = this;
+  //
+  //   // this.collection.where({number: })
+  //
+  //   console.log(this_destination);
 
     // var id = event.target.id;
     // console.log(id);
@@ -34,50 +35,50 @@ app.Flight_view = Backbone.View.extend({
     // var flightToDraw = this.collection.where({
     //   number: parseInt(id)
     // });
+    //
+    // something = app.flight_list.where({
+    // //   number: parseInt(id)
+    // })
 
-    something = app.flight_list.where({
-      number: parseInt(id)
-    })
-
-    var airplaneId = something[0].get("airplane_id")
-
-    console.log(airplaneId);
-
-    airplaneLayout = app.airplaneList.where({
-      id: airplaneId
-    })
-
-    console.log(airplaneLayout);
-
-    var row = airplaneLayout[0].get("row")
-    var column = airplaneLayout[0].get("column")
-
-    console.log(row);
-
-    console.log(column);
-
-    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var rows = row; // 1-30
-    var columns = column;
-    var result = "";
-    result += "<table border=1>";
-    result += "<tbody>";
-    for (var y = 0; y < columns; y++) {
-      result += "<tr>";
-      for (var x = 0; x < rows; x++) {
-        result += "<td class='seat-button' id='" + (y + 1) + alphabet[x] + "'>";
-        result += (y + 1) + alphabet[x];
-        result += "</td>";
-      }
-
-      result += "</tr>";
-    }
-    result += "</tbody>";
-    result += "</table>";
-    // this.$el.html(result);
-    $('#table').html(result);
-    console.log(result);
-  },
+  //   var airplaneId = something[0].get("airplane_id")
+  //
+  //   console.log(airplaneId);
+  //
+  //   airplaneLayout = app.airplaneList.where({
+  //     id: airplaneId
+  //   })
+  //
+  //   console.log(airplaneLayout);
+  //
+  //   var row = airplaneLayout[0].get("row")
+  //   var column = airplaneLayout[0].get("column")
+  //
+  //   console.log(row);
+  //
+  //   console.log(column);
+  //
+  //   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   var rows = row; // 1-30
+  //   var columns = column;
+  //   var result = "";
+  //   result += "<table border=1>";
+  //   result += "<tbody>";
+  //   for (var y = 0; y < columns; y++) {
+  //     result += "<tr>";
+  //     for (var x = 0; x < rows; x++) {
+  //       result += "<td class='seat-button' id='" + (y + 1) + alphabet[x] + "'>";
+  //       result += (y + 1) + alphabet[x];
+  //       result += "</td>";
+  //     }
+  //
+  //     result += "</tr>";
+  //   }
+  //   result += "</tbody>";
+  //   result += "</table>";
+  //   // this.$el.html(result);
+  //   $('#table').html(result);
+  //   console.log(result);
+  // },
 
 
 })
